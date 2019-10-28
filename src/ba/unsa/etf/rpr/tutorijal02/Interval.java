5aea32ef5de6a8587654b9609a12f8e3fad6d7d9
skahvedzic1 <skahvedzic1@etf.unsa.ba>
2019-10-28 20:37:56 +0100

    Final commit

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,41 +1,76 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
+import java.util.Objects;
 
 public class Interval {
-     double pocetna_tacka,krajnja_tacka;
-     boolean pripada_pocetna,pripada_krajnja;
+    private double pocetna_tacka;
+    private double krajnja_tacka;
+    private boolean pocetak_pripada;
+    private boolean kraj_pripada;
 
-     Interval(double pt, double kt, boolean pp,boolean pk) {
-
-         if(pocetna_tacka>krajnja_tacka) throw new IllegalAccessException("Pocetna tacka veca od krajnje!");
-         pocetna_tacka=pt;
-         krajnja_tacka=kt;
-         pripada_pocetna=pp;
-         pripada_krajnja=pk;
-     }
-
-     Interval() {
-
-     }
-
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        if (!super.equals(object)) return false;
-        Interval interval = (Interval) object;
-        return java.lang.Double.compare(interval.pocetna_tacka, pocetna_tacka) == 0 &&
-                java.lang.Double.compare(interval.krajnja_tacka, krajnja_tacka) == 0 &&
-                pripada_pocetna == interval.pripada_pocetna &&
-                pripada_krajnja == interval.pripada_krajnja;
+    public double getPocetna_tacka() {
+        return pocetna_tacka;
     }
 
-    @java.lang.Override
-    public java.lang.String toString() {
+    public double getKrajnja_tacka() {
+        return krajnja_tacka;
+    }
+
+    public Interval(double v, double v1, boolean b, boolean b1) {
+        if ( v > v1) throw new IllegalArgumentException();
+        pocetna_tacka = v;
+        krajnja_tacka = v1;
+        pocetak_pripada = b;
+        kraj_pripada = b1;
+    }
+
+    public Interval() {
+        this(0,0,false,false);
+    }
+
+    public static Interval intersect(Interval i, Interval i2) {
+        if (i2.getPocetna_tacka() > i.getPocetna_tacka() && i2.getPocetna_tacka() < i.getKrajnja_tacka()) {
+            return new Interval(i2.getPocetna_tacka(),i.getKrajnja_tacka(),true,true);
+        }
+        return null;
+    }
+
+    public Interval intersect(Interval interval) {
+        if(interval.getPocetna_tacka() > this.getPocetna_tacka() && interval.getPocetna_tacka() < this.getKrajnja_tacka()) {
+            return new Interval(interval.getPocetna_tacka(),this.getKrajnja_tacka(),true,true);
+        }
+        return null;
+    }
+    public boolean isIn(double v) {
+        if (getPocetna_tacka() > v && getKrajnja_tacka() < v) return true;
+        return false;
+    }
+
+    public boolean isNull() {
+        if (getKrajnja_tacka()== 0 && getPocetna_tacka()== 0) return true;
+        return false;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Interval interval = (Interval) o;
+        return Double.compare(interval.pocetna_tacka, pocetna_tacka) == 0 &&
+                Double.compare(interval.krajnja_tacka, krajnja_tacka) == 0 &&
+                pocetak_pripada == interval.pocetak_pripada &&
+                kraj_pripada == interval.kraj_pripada;
+    }
+
+    @Override
+    public String toString() {
         return "Interval{" +
-                "pocetna_tacka=" + pocetna_tacka +
-                ", krajnja_tacka=" + krajnja_tacka +
-                ", pripada_pocetna=" + pripada_pocetna +
-                ", pripada_krajnja=" + pripada_krajnja +
-                '}';
+                pocetna_tacka +
+                ", " + krajnja_tacka +
+                "} pocetak_pripada= " + pocetak_pripada +
+                " , kraj_pripada= " + kraj_pripada +
+                '.';
     }
+
 }
